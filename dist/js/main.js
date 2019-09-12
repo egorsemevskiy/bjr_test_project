1df797d8752bb6c0c39519031459911f18a9397f
egorsemevskiy <egor.semevskiy@yandex.ru>
2019-09-12 11:23:43 +0300

    Добавил бабел. Работает отсчет секунд

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,36 +3,15 @@
 var clock = document.querySelector('#clock');
 var clockTwelve = document.querySelector('#main-clock-12');
 var clockTwentyFour = document.querySelector('#main-clock-24');
-var currentTimeLeftInSession = 60000;
+var currentTimeLeftInSession = 60;
 var xhr = new XMLHttpRequest();
 var dateTimeLink = 'http://worldtimeapi.org/api/timezone/Europe/Moscow';
-clockTwelve.addEventListener('click', function () {
-  toggleClock();
-});
-clockTwentyFour.addEventListener('click', function () {
-  toggleClock();
-});
 document.addEventListener("DOMContentLoaded", function (event) {
-  clockTimer = setInterval(function () {
+  var clockTimer = setInterval(function () {
     currentTimeLeftInSession--;
     console.log(currentTimeLeftInSession);
   }, 1000);
 });
-
-var toggleClock = function toggleClock(reset) {
-  if (reset) {// STOP THE TIMER
-  } else {
-    if (isClockRunning === true) {
-      // PAUSE THE TIMER
-      isClockRunning = false;
-    } else {
-      // START THE TIMER
-      isClockRunning = true;
-    }
-  }
-};
-
-console.log(currentTimeLeftInSession);
 xhr.open('GET', dateTimeLink, false);
 xhr.send();
 
