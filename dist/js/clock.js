462d651948d08fd5508367505dd16907fb15d6c1
egorsemevskiy <egor.semevskiy@yandex.ru>
2019-09-12 18:52:13 +0300

    Добавлены аналоговые часы.

--- dist/js/clock.js
+++ dist/js/clock.js
@@ -1,73 +1,3 @@
-
-
-const clock = document.querySelector('#clock');
-const clockTwelve = document.querySelector('#main-clock-12');
-const clockTwentyFour = document.querySelector('#main-clock-24');
-const analogClock = document.querySelector("#myClock");
-
-let a = false;
-let xhr = new XMLHttpRequest();
-let dateTimeLink = 'http://worldtimeapi.org/api/timezone/Europe/Moscow';
-
-
-let BjrClocks = {
-   
-    initialize: () => {
-        BjrClocks.clockAction();
-
-        clockTwelve.addEventListener('click', () => {
-            a = true;
-            BjrClocks.clockAction;
-        });
-        clockTwentyFour.addEventListener('click', () => {
-            a = false;
-            BjrClocks.clockAction;
-        });
-
-        
-    },
-
-    clockAction: () => {
-        let date = new Date(),
-        hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
-        minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
-        seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
-        BjrClocks.printClock(hours, minutes, seconds);
-    },
-
- 
-    printClock: (h,m,s) => {
-    
-        if (a == true){
-            let ampm = h >= 12 ? 'pm' : 'am';   
-            h = h % 12;
-            h = h ? h : 12; 
-             clock.innerHTML = (h +":"+m+":"+s +" " + ampm)
-        }else {
-             clock.innerHTML = (h +":"+m+":"+s);
-        }
-    }
-    
-};
-
-let AjaxDateTime ={
-    initialize:()=>{
-        xhr.open('GET', dateTimeLink, false);
-        xhr.send();
-        if (xhr.status != 200) {
-            console.log( xhr.status + ': ' + xhr.statusText );
-        } else {
-            AjaxDateTime.displayCurrentTime( xhr.responseText ); 
-        }
-    },
-
-    displayCurrentTime: (text)=> {
-        let time =  JSON.parse(text).unixtime;
-         console.log(time);
-    }
-}
-
- 
 let analogClockController = {
     initialize: () =>{
        setInterval(analogClockController.buildClock,1000)
@@ -79,20 +9,19 @@ let analogClockController = {
     },
 
     displayCanvas: () => {
-       
-       let contextHTML = analogClock.getContext('2d');
- 
+       let contextHTML = canvasHTML.getContext('2d');
+       let canvasHTML = document.getElementById('myCanvas');
   
-    contextHTML.strokeRect(0,0,analogClock.width, analogClock.height);
+    contextHTML.strokeRect(0,0,canvasHTML.width, canvasHTML.height);
 	
     //Расчет координат центра и радиуса часов
-    let radiusClock = analogClock.width/2 - 10;
-    let xCenterClock = analogClock.width/2;
-    let yCenterClock = analogClock.height/2;
+    let radiusClock = canvasHTML.width/2 - 10;
+    let xCenterClock = canvasHTML.width/2;
+    let yCenterClock = canvasHTML.height/2;
 	
     //Очистка экрана. 
     contextHTML.fillStyle = "#ffffff";
-    contextHTML.fillRect(0,0,analogClock.width,analogClock.height);
+    contextHTML.fillRect(0,0,canvasHTML.width,canvasHTML.height);
 	
     //Рисуем контур часов
     contextHTML.strokeStyle =  "#000000";
@@ -183,12 +112,3 @@ let analogClockController = {
 
     }
 }
-
-
-document.addEventListener("DOMContentLoaded", function() { 
-    BjrClocks.initialize();
-    AjaxDateTime.initialize();
-    analogClockController.initialize();
-    setInterval(BjrClocks.clockAction,1000);
-    setInterval( analogClockController.displayCanvas,1000);
-});         
