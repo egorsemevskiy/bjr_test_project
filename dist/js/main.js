5beab4e226744ae6a52abb1ba99d7e0767bf9d55
egorsemevskiy <egor.semevskiy@yandex.ru>
2019-09-12 11:50:24 +0300

    Вывел на экран текст часов

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,20 +3,30 @@
 var clock = document.querySelector('#clock');
 var clockTwelve = document.querySelector('#main-clock-12');
 var clockTwentyFour = document.querySelector('#main-clock-24');
-var currentTimeLeftInSession = 60;
 var xhr = new XMLHttpRequest();
 var dateTimeLink = 'http://worldtimeapi.org/api/timezone/Europe/Moscow';
-document.addEventListener("DOMContentLoaded", function (event) {
-  var clockTimer = setInterval(function () {
-    currentTimeLeftInSession--;
-    console.log(currentTimeLeftInSession);
-  }, 1000);
-});
 xhr.open('GET', dateTimeLink, false);
 xhr.send();
 
 if (xhr.status != 200) {
   console.log(xhr.status + ': ' + xhr.statusText);
 } else {
-  console.log(xhr.responseText);
+  displayCurrentTime(xhr.responseText);
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  setInterval(clockAction, 1000);
+});
+
+function displayCurrentTime(text) {
+  console.log(text);
+}
+
+function clockAction() {
+  var date = new Date(),
+      hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours(),
+      minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes(),
+      seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+  clock.innerHTML = hours + ":" + minutes + ":" + seconds;
+  console.log(hours + ':' + minutes + ':' + seconds);
 }
